367fb0cb5d7c5267514d9518b06bee143fb83a99
Jeremy Kitchen <kitchen@kitchen.io>
2020-08-17 20:59:04 -0700

    set EXTRAKEY_ENABLE = yes for custom pinky
    I thought I'd done this but apparently when I moved it to this rules.mk
    I just copied it verbatim and didn't change it to yes and so I undid the
    changes I made to enable it in the first place. Boom.

--- keyboards/pinky/3/keymaps/kitchen_pinky3_dvorak/rules.mk
+++ keyboards/pinky/3/keymaps/kitchen_pinky3_dvorak/rules.mk
@@ -1,3 +1,3 @@
 # I use the volume controls in my layout, so override this here
 
-EXTRAKEY_ENABLE = no       # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
