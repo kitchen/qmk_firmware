266dac213831c30efbdb851a61afc2ee12c3dbd5
Jeremy Kitchen <kitchen@kitchen.io>
2020-10-10 23:00:19 -0700

    bongocat wpm meter
    works like a champ. Though I'd like to see if I can get my led
    animations back and get rid of the wpm and just have a keystroke
    counter. I'll look at the wpm code to see how it counts things and
    simplify that to just a straight up counter with a timeout (probably
    when bongocat goes back to rest state, or when the OLEDs got to rest
    state) that saves the counter to eeprom so it doesn't get lost between reboots.

--- keyboards/kyria/keymaps/kitchen-dvorak/rules.mk
+++ keyboards/kyria/keymaps/kitchen-dvorak/rules.mk
@@ -1,3 +1,11 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes	       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+WPM_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+AUDIO_ENABLE = no
+
+EXTRAFLAGS += -flto
